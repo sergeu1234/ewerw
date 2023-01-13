ab8dbaed69514c95a4691799f2dd7bd706ae47ad
swer <you@youraddress.com>
2023-01-13 22:51:27 +0300

    dsf

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,11 +15,7 @@ SECRET_KEY = 'django-insecure-9o^!5shqqavj@#@&36+y+nl3csjw2%hr3(wrzh@be05fd5ita)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'jameswaweru.herokuapp.com',
-    'jameswaweruweb.herokuapp.com',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['werer-werewr-werewr.onrender.com']
 
 # Application definition
 
